d04e1241c41f56bf03d521297773231e449b3819
wei-m-teh <wei.m.teh@gmail.com>
2019-11-07 11:19:10 -0600

    initial revision

---
+++ word2vec.py
@@ -0,0 +1,89 @@
+from keras.models import Model
+from keras.layers import Input, Dense, Reshape, merge
+from keras.layers.embeddings import Embedding
+from keras.preprocessing.sequence import skipgrams
+from keras.preprocessing import sequence
+
+import urllib.request
+import collections
+import math
+import os
+import random
+import zipfile
+import datetime as dt
+
+import numpy as np
+import tensorflow as tf
+
+# Read the data into a list of strings.
+def read_data(filename):
+    """Extract the first file enclosed in a zip file as a list of words."""
+    with zipfile.ZipFile(filename) as f:
+        data = tf.compat.as_str(f.read(f.namelist()[0])).split()
+    return data
+
+def maybe_download(filename, url, expected_bytes):
+    """Download a file if not present, and make sure it's the right size."""
+    if not os.path.exists(filename):
+        filename, _ = urllib.request.urlretrieve(url + filename, filename)
+    statinfo = os.stat(filename)
+    if statinfo.st_size == expected_bytes:
+        print('Found and verified', filename)
+    else:
+        print(statinfo.st_size)
+        raise Exception(
+            'Failed to verify ' + filename + '. Can you get to it with a browser?')
+    return filename
+
+def build_dataset(words, n_words):
+    """Process raw inputs into a dataset."""
+    count = [['UNK', -1]]
+    count.extend(collections.Counter(words).most_common(n_words - 1))
+    dictionary = dict()
+    for word, _ in count:
+        dictionary[word] = len(dictionary)
+    data = list()
+    unk_count = 0
+    for word in words:
+        if word in dictionary:
+            index = dictionary[word]
+        else:
+            index = 0  # dictionary['UNK']
+            unk_count += 1
+        data.append(index)
+    count[0][1] = unk_count
+    reversed_dictionary = dict(zip(dictionary.values(), dictionary.keys()))
+    return data, count, dictionary, reversed_dictionary
+
+def collect_data(vocabulary_size=10000):
+    url = 'http://mattmahoney.net/dc/'
+    filename = maybe_download('text8.zip', url, 31344016)
+    vocabulary = read_data(filename)
+    print(vocabulary[:7])
+    data, count, dictionary, reverse_dictionary = build_dataset(vocabulary,
+                                                                vocabulary_size)
+    del vocabulary  # Hint to reduce memory.
+    return data, count, dictionary, reverse_dictionary
+
+vocab_size = 10000
+data, count, dictionary, reverse_dictionary = collect_data(vocabulary_size=vocab_size)
+print("Data collected!")
+
+window_size = 3
+vector_dim = 300
+epochs = 1000000
+
+valid_size = 16     # Random set of words to evaluate similarity on.
+valid_window = 100  # Only pick dev samples in the head of the distribution.
+# Pick 16 random number out of 100.
+valid_examples = np.random.choice(valid_window, valid_size, replace=False)
+print("Done!")
+
+
+sampling_table = sequence.make_sampling_table(vocab_size)
+couples, labels = skipgrams(data, vocab_size, window_size=window_size, sampling_table=sampling_table)
+word_target, word_context = zip(*couples)
+word_target = np.array(word_target, dtype="int32")
+word_context = np.array(word_context, dtype="int32")
+
+print(couples[:10], labels[:10])
